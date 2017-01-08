a8bfea6e939441b4382851b09c04aaa53ecb1575
hugollopes <hugo.l.lopes@gmail.com>
2017-01-08 17:02:26 +0100

    Add files via upload

---
+++ console.py
@@ -0,0 +1,38 @@
+import pickle
+import sys
+
+dControlData = {}
+
+#for windows:
+#strControlFile = "C:\\Users\\TGDLOHU1\\pythoncode\\controlaction.txt"
+#for within docker image
+strControlFile = "/mnt/pythoncode/controlaction.txt"
+
+dControlData = { "Action": "nothing", "AttackNumber": 300 }
+
+
+
+#controlFile = open( strControlFile, "rb" )
+#dControlData = pickle.load( controlFile )
+#print (dControlData['Action'])
+#controlFile.close()
+
+sCommands ="(a)ttack,capture,capturepet,detectpet,exit or ctlr-c':"
+print(sCommands)
+command = ""
+while str(command) != 'wow':
+    command = raw_input(">")#for widows, use input.
+    if command == "a":
+        command = "attack"
+    #if command == "detectpet"
+        
+    dControlData['Action'] = str(command)
+    controlFile = open( strControlFile, "wb" )
+    pickle.dump( dControlData, controlFile ,protocol=2)
+    controlFile.close()
+
+    
+
+
+
+
