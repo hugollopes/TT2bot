628870476f4e8cdfb038e5588c4c1395745333d4
hugollopes <hugo.l.lopes@gmail.com>
2017-01-30 01:52:14 +0100

    more stuff

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,70 @@ FROM gcr.io/tensorflow/tensorflow:latest
 MAINTAINER Hugo Lopes <hugo.l.lopes@google.com>
 
 # Pillow needs libjpeg by default as of 3.0.
-RUN apt-get update && apt-get install -y --no-install-recommends \
-        libjpeg8-dev \
-        && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+#RUN apt-get update && apt-get install -y --no-install-recommends \
+#        libjpeg8-dev \
+#        && \
+#    apt-get clean && \
+#    rm -rf /var/lib/apt/lists/*
 
 RUN pip install scikit-learn pyreadline Pillow
+
+RUN sudo apt-get update
+#RUN apt-get -y install tesseract-ocr  libtesseract-dev libleptonica-dev
+RUN pip install cython
+RUN apt-get -y install wget tar ca-certificates
+RUN apt-get install -y \
+  autoconf \
+  automake \
+  autotools-dev \
+  build-essential \
+  checkinstall \
+  libjpeg-dev \
+  libpng-dev \
+  libtiff-dev \
+  libtool \
+  python \
+  python-imaging \
+  python-tornado \
+  wget \
+  zlib1g-dev
+
+#RUN sudo apt-get  -y install python-distutils-extra tesseract-ocr tesseract-ocr-eng libopencv-dev libtesseract-dev libleptonica-dev python-all-dev swig libcv-dev python-opencv python-
+numpy python-setuptools build-essential subversion
+#RUN sudo apt-get -y install autoconf automake libtool
+#RUN sudo apt-get -y install libpng12-dev libjpeg62-dev libtiff4-dev zlib1g-dev
+#RUN sudo apt-get -y install checkinstall
+
+
+RUN mkdir ~/temp \
+  && cd ~/temp/ \
+  && wget http://www.leptonica.com/source/leptonica-1.73.tar.gz \
+  && tar -zxvf leptonica-1.73.tar.gz \
+  && cd leptonica-1.73 \
+  && ./configure \
+  && make \
+  && checkinstall \
+  && ldconfig
+
+RUN cd ~/temp/ \
+  && wget https://github.com/tesseract-ocr/tesseract/archive/3.04.01.tar.gz \
+  && tar -zxvf 3.04.01.tar.gz \
+  && cd tesseract-3.04.01 \
+  && ./autogen.sh \
+  && mkdir ~/local \
+  && ./configure --prefix=$HOME/local/ \
+  && make \
+  && make install
+
+RUN sudo ldconfig
+ENV PATH="/usr/local/lib:${PATH}"
+RUN sudo apt-get install tesseract-ocr-eng
+ENV PKG_CONFIG_PATH="/root/local/lib/pkgconfig"
+RUN CPPFLAGS=-I/root/local/lib/pkgconfig pip install tesserocr
+ENV LD_LIBRARY_PATH="/root/local/lib:${LD_LIBRARY_PATH}"
+ENV TESSDATA_PREFIX="/usr/share/tesseract-ocr/tessdata"
+RUN sudo ln -s  /root/local/lib/libtesseract.so.3 /usr/local/lib/libtesseract.so.3 #symlink
+
 # keep original TF notebooks #RUN rm -rf /notebooks/*
 #dont add #ADD *.ipynb /notebooks/
 #WORKDIR /notebooks
--- TrainConsole.py
+++ TrainConsole.py
@@ -1,21 +1,68 @@
 from __future__ import print_function
 from genericTrain import TT2Predictor , TrainerPredictor
+import globals as glo
 
+
+from PIL import Image
+#import pytesseract
+import tesserocr
+from tesserocr import PyTessBaseAPI
+print (tesserocr.tesseract_version() )
+print(tesserocr.get_languages())
+images = [glo.UNCLASSIFIED_GLOBAL_CAPTURES_FOLDER + '/fullcapture961 .png']
+
+#with PyTessBaseAPI() as api:
+#    for img in images:
+#        api.SetImageFile(img)
+#        print( api.GetUTF8Text())
+#        print( api.AllWordConfidences())
+
+img = Image.open(glo.DATA_FOLDER + '/number_range_predictorcropped3.png')#glo.UNCLASSIFIED_GLOBAL_CAPTURES_FOLDER + '/fullcapture961 .png')
+#img = img.convert('L')
+
+from tesserocr import PyTessBaseAPI, RIL, iterate_level
+
+with PyTessBaseAPI() as api:
+    api.SetImageFile(glo.DATA_FOLDER + '/number_range_predictorcropped3.png')
+    #api.SetVariable("tessedit_pageseg_mode", "10")
+    print("recognized txt:",api.GetUTF8Text().encode('utf-8').strip())
+    #api.Recognize()
+
+    ri = api.GetIterator()
+    level = RIL.SYMBOL
+    for r in iterate_level(ri, level):
+        symbol = r.GetUTF8Text(level)  # r == ri
+        conf = r.Confidence(level)
+        print(u'symbol {}, conf: {}'.format(symbol, conf).encode('utf-8').strip())
+        indent = False
+        ci = r.GetChoiceIterator()
+        for c in ci:
+            if indent:
+                print('\t\t ',)
+            print('\t- ',)
+            choice = c.GetUTF8Text()  # c == ci
+            print(            u'{} conf: {}'.format(choice, c.Confidence()).encode('utf-8').strip())
+            indent = True
+
+
+    #print("aquiiii", tesserocr.image_to_text(img))
 
 
 predictor = TT2Predictor()
 print("creating new trainer")
 new_predictor = TrainerPredictor("tab_predictor", ["skills_tab", "heroes_tab", "equipment_tab",
-                                                   "pet_tab", "relic_tab", "shop_tab", "no_tab"]
-                                            , (51, 1, 59, 717)
-                                            , 2, 179, 255.0
-                                            , [200, 30])
+                                                           "pet_tab", "relic_tab", "shop_tab", "no_tab"]
+                                         , (51, 1, 59, 717)
+                                         , 2, 179, 255.0
+                                         , [200, 30])
+
 print("trainer created")
 #todo: need a capture and parse
+#todo: need a number by time on created crops or copying the original name.
 #new_predictor.crop_images(selected_globals=True)
-new_predictor.process_images()
-new_predictor.read_and_pickle()
-new_predictor.train_graph()
+#new_predictor.process_images()
+#new_predictor.read_and_pickle()
+#new_predictor.train_graph()
 """
 
 predictor.parse_raw_image()
